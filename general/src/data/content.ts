52c841f39c870ce0311f79777bb34cd2ee0054fc
litmus-zhang <abdulsalamlukmon9@gmail.com>
2025-05-11 15:14:08 +0100

    added dynage lms and marketplace

---
+++ general/src/data/content.ts
@@ -0,0 +1,41 @@
+import Facebook from "../assets/facebook.svg";
+import Linkedin from "../assets/linkedin.svg";
+import Twitter from "../assets/twitter.svg";
+
+export const logos = [
+    { name: "Facebook", src: Facebook, link: "https://facebook.com/dynage" },
+    { name: "Linkedin", src: Linkedin, link: "https://linkedin.com/company/dynage" },
+    { name: "Twitter", src: Twitter, link: "https://twitter.com/_dynage" },
+  ];
+
+
+export const navLinks = [
+    {
+      name: "About us",
+      href: "#about",
+    },
+    {
+      name: "Engineering",
+      href: "https://engineering.dynage.technology",
+    },
+    {
+      name: "Studio",
+      href: "https://studio.dynage.technology",
+    },
+    {
+      name: "Academy",
+      href: "https://academy.dynage.technology",
+    },
+    {
+      name: "Research",
+      href: "https://research.dynage.technology",
+    },
+    {
+      name: "Finance",
+      href: "https://finance.dynage.technology",
+    },
+    {
+      name: "Contact",
+      href: "#contact",
+    },
+  ];
